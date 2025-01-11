3828a55964b2c2c6fe6f373b7b38bbd2ba29853a
ikshitd <dhy.ikshit009@gmail.com>
2025-01-11 11:47:55 +0530

    chore: Cleaning and Adding Prettier Config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1> Hey !! Excited to begin on this new project !! </h1> // for testing
     </div>
   );
