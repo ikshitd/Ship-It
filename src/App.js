b95f8d04020378c2bac70e87a63d35131ee358b1
ikshitd <dhy.ikshit009@gmail.com>
2025-01-12 22:16:08 +0530

    feat: Adding Navbar and updating some styling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Login from './components/Pages/Login';
 import Register from './components/Pages/Register';
 import Home from './components/Pages/Home';
 import Board from './components/Board/Board.js';
+import Navbar from './components/Bar/Navbar.js';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
@@ -13,8 +14,7 @@ function App() {
         <Route path="/register" element={<Register />}></Route>
         <Route path="/test" element={<Board />}></Route>
       </Routes>
+      <Navbar />
     </Router>
   );
 }
-
-export default App;
