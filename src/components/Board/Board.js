6bb519ec9d04d6905d0ec19261aa586d0c671fc5
Ikshit Dhyani <ikshitdhiyani@Ikshits-MacBook-Air.local>
2025-01-15 22:51:17 +0530

    chore: Using @hello-pangea/dnd in place of react-beautiful-dnd for smoother and better drag/drop context

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,4 +1,4 @@
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import Task from './Task';
 import { Button } from 'antd';
 import { useAppContext } from '../../context/Context';
